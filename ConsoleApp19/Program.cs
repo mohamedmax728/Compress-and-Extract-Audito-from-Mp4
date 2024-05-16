d7c0a2d191e93f89acafddb4ab4b1dc7c20ad1f4
Mohamed Abo ElDahab <M.aboeldahab@arco.sa>
2024-05-16 18:59:37 +0300

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NReco.VideoConverter;
+Console.WriteLine("Hello, World!");
+string inputFilePath = "D:\\New folder (13)\\lv_0_20240516150810.mp4";
+string outputFilePath = "D:\\New folder (13)\\lv_0_22.H264";
+string audioFilePath = "D:\\New folder (13)\\lv_0_222.mp3";
+
+try
+{
+    //await Task.WhenAll(
+    //    ExtractAudioAsync(inputFilePath, audioFilePath)
+    //   , CompressVideoAsync(inputFilePath, outputFilePath)
+    //    );
+    ExtractAudioAsync(inputFilePath, audioFilePath);
+    CompressVideoAsync(inputFilePath, outputFilePath);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+static async Task CompressVideoAsync(string inputFile, string outputFile)
+{
+    //FileStream fs = File.Create(outputFile);
+    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+    ffMpeg.ConvertMedia(inputFile, outputFile, Format.h264);
+}
+static async Task ExtractAudioAsync(string inputFile, string audioFilePath)
+{
+    //FileStream fs = File.Create(outputFile);
+    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+    ffMpeg.ConvertMedia(inputFile, audioFilePath, "mp3");
+}
